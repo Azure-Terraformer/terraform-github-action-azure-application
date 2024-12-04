55514f962e7419d0be731053a813736d96ed3923
= <=>
2024-12-04 09:32:11 -0500

    replace TFTPL

--- main.tf
+++ main.tf
@@ -22,22 +22,28 @@ resource "github_repository_file" "bulk" {
 
 }
 
+locals {
+  original_yaml    = file("${path.module}/files/.github/workflows/atat-pull-request-terraform-plan.yaml")
+  escaped_yaml     = replace(original_yaml, "$${", "$$$${")
+  tf_template_yaml = replace(escaped_yaml, "$TFTPL", "$")
+}
+
 resource "github_repository_file" "pull_request_plan" {
 
   count = length(var.environments)
 
-  repository = var.repository
-  branch     = var.branch
-  file       = ".github/workflows/atat-pull-request-plan-terraform-plan-${var.environments[count.index]}.yaml"
-  content = templatefile(
-    "${path.module}/files/.github/workflows/atat-pull-request-terraform-plan.yaml",
-    {
-      environment_name = "${var.environments[count.index]}"
-    }
-  )
+  repository          = var.repository
+  branch              = var.branch
+  file                = ".github/workflows/atat-pull-request-plan-terraform-plan-${var.environments[count.index]}.yaml"
   commit_message      = "Managed by Terraform"
   commit_author       = var.commit_user.name
   commit_email        = var.commit_user.email
   overwrite_on_create = true
+
+  content = templatestring(tf_template_yaml,
+    {
+      environment_name = "${var.environments[count.index]}"
+    }
+  )
 
 }
